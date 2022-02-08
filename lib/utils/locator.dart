de523af3620a1c81abc65dca2fc43c097afb397f
Priscilla Achizue <Priscilla@Priscillas-MacBook.local>
2022-02-08 04:45:22 +0100

    Registers dependencies

--- lib/utils/locator.dart
+++ lib/utils/locator.dart
@@ -1,10 +1,44 @@
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:volt_driver/data/local/local.dart';
+import 'package:volt_driver/data/remote/auth_apis/auth_repo.dart';
+import 'package:volt_driver/data/remote/auth_apis/auth_repo_impl.dart';
+import 'package:volt_driver/data/remote/auth_apis/auth_service.dart';
+import 'package:volt_driver/data/remote/auth_apis/auth_service_impl.dart';
 import 'package:volt_driver/handlers/handlers.dart';
 
 GetIt locator = GetIt.instance;
 
 Future<void> setupLocator({String baseApi = ''}) async {
+  final sharedPreferences = await SharedPreferences.getInstance();
+  locator.registerSingleton(sharedPreferences);
+  //Local storage
+  locator.registerLazySingleton<SecureStorage>(
+    () => SecureStorageImpl(),
+  );
+
+  locator.registerLazySingleton<LocalCache>(
+    () => LocalCacheImpl(
+      sharedPreferences: locator(),
+      storage: locator(),
+    ),
+  );
+
+//handlers
   locator
       .registerLazySingleton<NavigationHandler>(() => NavigationHandlerImpl());
   locator.registerLazySingleton<DialogHandler>(() => DialogHandlerImpl());
+
+  //services
+
+  locator.registerLazySingleton<AuthService>(
+    () => AuthServiceImpl(
+      authRepo: locator(),
+      cache: locator(),
+    ),
+  );
+  //repos
+  locator.registerLazySingleton<AuthRepository>(
+    () => AuthRepositoryImpl(baseApi),
+  );
 }
