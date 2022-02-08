bf11fabced03e0de17cb1612d20fca8223a74204
Priscilla Achizue <Priscilla@Priscillas-MacBook.local>
2022-02-08 04:30:31 +0100

    Splash screen logic

--- lib/presentation/viewmodels/splash_screen_view_model.dart
+++ lib/presentation/viewmodels/splash_screen_view_model.dart
@@ -4,7 +4,28 @@ import 'package:volt_driver/utils/constants.dart';
 class SplashScreenVM extends BaseViewModel {
   Future<void> handleNavigation() async {
     await Future.delayed(const Duration(seconds: 2)).then((value) async {
-      navigationHandler.pushReplacementNamed(onboardingViewRoute);
+      await localCache.getToken().then(
+        (token) async {
+          //check if user is logged in
+          if (token != null && token.isNotEmpty) {
+            //if user is logged in,
+            navigationHandler.pushReplacementNamed(homeViewRoute);
+          } else {
+            //#beginning of user not logged in flow
+            //if user is not logged in, check if user has been onboarded
+            bool hasBeenOnboarded =
+                localCache.getFromLocalCache(onboardingDone) != null;
+
+            if (hasBeenOnboarded) {
+              navigationHandler.pushReplacementNamed(logInViewRoute);
+            } else {
+              //if user has not been onboarded, navigate to OnboardingView
+              navigationHandler.pushReplacementNamed(onboardingViewRoute);
+            }
+            //#end of user not logged in flow
+          }
+        },
+      );
     });
   }
 }
